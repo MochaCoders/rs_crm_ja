2d81e93606c019f128fd7a6c87f733e675d8e222
Directorfin <connect@mochacoders.com>
2025-09-07 11:11:27 -0500

    emails generated from template

---
+++ resources/views/emails/partials/appointment-button.blade.php
@@ -0,0 +1,10 @@
+<a href="{{ $url }}"
+   style="display:inline-block;
+          padding:10px 20px;
+          color:#ffffff;
+          background-color:#3490dc;
+          border-radius:4px;
+          text-decoration:none;
+          font-weight:bold;">
+    Set Appointment
+</a>
